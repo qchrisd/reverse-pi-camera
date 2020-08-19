e0ec228c20e05d5675ace896c673201b6e902f1e
qchrisd <q.chris.d@gmail.com>
2020-08-19 17:40:41 -0400

    Played with some event handlers and cleaned the code for more clarity

--- pygametest.py
+++ pygametest.py
@@ -1,25 +1,36 @@
 # Import dependencies
 import pygame
 import pygame.camera
+import sys
 
 # Initialize Pygame and create the screen (window)
 pygame.init()
-screen = pygame.display.set_mode((600,440))
+window = pygame.display.set_mode((640,480))
 
 # Initialize the camera and start it for image capturing
 pygame.camera.init()
-cam = pygame.camera.Camera('/dev/video0', (640, 480), 'RGB')
+cam = pygame.camera.Camera('/dev/video0', (640, 480))
 cam.start()
 
 # Create the pygame surface (unsure what this does)
-surface = pygame.Surface((640,480))
+frame = pygame.Surface((640,480))
 
 while True:
+    # Handles events
+    for event in pygame.event.get():
+        # If the program was ended then exit the application
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            print(f"The key {event.key} was pressed. {event.unicode}")
+        elif event.type == pygame.KEYUP:
+            print(f"The key {event.key} was released. {event.mod}")
+            
     # Gets an image and puts it on the previously created surface
-    cam.get_image(surface)
+    cam.get_image(frame)
     
     # blits the surface onto the screen at the orign of the window
-    screen.blit(surface, (0,0))
+    window.blit(frame, (0,0))
 
     # Sends the blitted things to the frame buffor for display
     pygame.display.update()
