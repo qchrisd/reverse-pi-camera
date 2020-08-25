f69c1a83aad2f622aaa4d8ef15a744a077d9911d
qchrisd <q.chris.d@gmail.com>
2020-08-25 17:02:17 -0400

    Added distance sensing and text overlay to the test camera

--- pygametest.py
+++ pygametest.py
@@ -1,7 +1,10 @@
 # Import dependencies
 import pygame
 import pygame.camera
+import pygame.font
 import sys
+import board
+import adafruit_hcsr04 as hcsr
 
 # Initialize Pygame and create the screen (window)
 pygame.init()
@@ -14,26 +17,39 @@ print(cameras)
 cam = pygame.camera.Camera(cameras[0], (640, 480))
 cam.start()
 
+# Create a font object to hold text
+pygame.font.init()
+d1 = pygame.font.Font(None, 50)
+
 # Create the pygame surface (unsure what this does)
 frame = pygame.Surface((640,480))
 
-while True:
-    # Handles events
-    for event in pygame.event.get():
-        # If the program was ended then exit the application
-        if event.type == pygame.QUIT:
-            sys.exit()
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+# initialize the distance sensor for proper GPIO pin release
+with hcsr.HCSR04(trigger_pin=board.D16, echo_pin=board.D20) as sonar:
+    while True:
+        # Handles events
+        for event in pygame.event.get():
+            # If the program was ended then exit the application
+            if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.key == pygame.K_f:
-                pygame.display.toggle_fullscreen()
-            
-    # Gets an image and puts it on the previously created surface
-    cam.get_image(frame)
-    
-    # blits the surface onto the screen at the orign of the window
-    window.blit(frame, (0,0))
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    sys.exit()
+                elif event.key == pygame.K_f:
+                    pygame.display.toggle_fullscreen()
+                
+        # Gets an image and puts it on the previously created surface
+        cam.get_image(frame)
+        
+        # Gets the distance from the US sensor
+        distance = sonar.distance
+        
+        # Writes distance to the frame
+        text = d1.render("{0:.0f}".format(distance), False, (255,255,255))
+        frame.blit(text, (50,50))
+        
+        # blits the surface onto the screen at the orign of the window
+        window.blit(frame, (0,0))
 
-    # Sends the blitted things to the frame buffor for display
-    pygame.display.update()
+        # Sends the blitted things to the frame buffor for display
+        pygame.display.update()
